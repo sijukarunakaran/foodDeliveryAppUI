02230479b02b34dc90d5e115c674834a5ddf6bef
sijukarunakaran <siju.karunakaran@gmail.com>
2020-12-25 15:46:53 +0530

    Project initialized

---
+++ FoodDelivery/FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Siju Karunakaran on 25/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
